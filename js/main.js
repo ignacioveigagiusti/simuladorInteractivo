c942976ee279ac497d6799f571b13e680e2b43f3
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2021-11-03 01:26:42 -0300

    Clase signal creada

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-let spl1 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la primera señal:"));
-let spl2 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la primera señal:"));
+//PARA TRABAJAR CON OBJETOS: Los objetos serán señales que incluirán:
+// Nivel de Potencia o Presión / Distancia del Receptor / Factor de Directividad / Retraso Inicial
+
+class signal {
+    constructor(soundPower, distance, Qfactor, timeDelay){
+        this.soundPower = parseFloat(soundPower);
+        this.distance = parseFloat(distance);
+        this.Qfactor = parseFloat(Qfactor);
+        this.timeDelay = parseFloat(timeDelay);
+        this.soundPowerLevel = 10*Math.log10(soundPower/10**(-12))
+        this.soundPressureLevel = this.soundPowerLevel - 10 * Math.log10((4*Math.PI*(distance**2))/(Qfactor));
+        this.soundPressure = ( 10**(this.soundPressureLevel/20) ) * 2 * (10**(-5));
+    }
+}
+
+// PRUEBA DE DATOS
+
+const signal1 = new signal(10, 2, 1, 0)
+console.log(signal1.soundPressureLevel)
+
+//PARA TRABAJAR CON ARRAYS: Operación aparte con frecuencias específicas pusheadas al array
+
+
+
+let soundPressureLevel1 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la primera señal:"));
+let soundPressureLevel2 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la primera señal:"));
 let phasediff = parseFloat(prompt("Ingrese la diferencia de fase (sólo necesaria si desea realizar una suma correlacionada):"));
 
 function correlatedsum(spl1,spl2,phasediff){
@@ -17,4 +41,6 @@ function noncorrelatedsum(spl1,spl2){
     let noncorsumalert = 'La suma de los niveles es: ' + noncorsum + ' dB';
     alert(noncorsumalert)
 }
+
+
 
