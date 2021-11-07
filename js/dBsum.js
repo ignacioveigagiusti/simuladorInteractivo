e16e07ebeb036fcf35e06365767776b52c05caff
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2021-11-07 20:04:58 -0300

    Interactuar con HTML

--- js/dBsum.js
+++ js/dBsum.js
@@ -1,12 +1,15 @@
-let soundPressureLevel1 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la primera señal:"));
-while (isNaN(soundPressureLevel1)) {
-    soundPressureLevel1 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la primera señal (Debe ser un número!):"));
+let soundPressureLevel1 = parseFloat(document.getElementById("soundPLevel1").value);
+let soundPressureLevel2 = parseFloat(document.getElementById("soundPLevel2").value);
+let phaseDifference = parseFloat(document.getElementById("phaseDifference").value);
+
+if (isNaN(soundPressureLevel1) && soundPressureLevel1 != null) {
+    document.getElementById("soundPLevel1").value = 0;
 }
-let soundPressureLevel2 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la segunda señal:"));
-while (isNaN(soundPressureLevel2)) {
-    soundPressureLevel2 = parseFloat(prompt("Ingrese el nivel de presión sonora en decibeles de la segunda señal (Debe ser un número!):"));
+
+if (isNaN(soundPressureLevel2) && soundPressureLevel2 != null) {
+    document.getElementById("soundPLevel2").value = 0;
 }
-let phaseDifference = parseFloat(prompt("Ingrese la diferencia de fase (sólo necesaria si desea realizar una suma correlacionada):"));
-while (isNaN(phaseDifference)) {
-    phaseDifference = parseFloat(prompt("Ingrese la diferencia de fase (sólo necesaria si desea realizar una suma correlacionada) (Debe ser un número!):"));
+
+if (isNaN(phaseDifference) && phaseDifference != null) {
+    document.getElementById("phaseDifference").value = 0;
 }
--- js/signalsum.js
+++ js/signalsum.js
@@ -1,51 +1,7 @@
-let soundPower1 = parseFloat(prompt("Ingrese el nivel de potencia sonora en Watts de la primera señal:"));
-while (isNaN(soundPower1)) {
-    soundPower1 = parseFloat(prompt("Ingrese el nivel de potencia sonora en Watts de la primera señal (Debe ser un número!):"));
-}
-let distance1 = parseFloat(prompt("Ingrese la distancia en metros de la fuente de la primera señal al receptor:"));
-while (isNaN(distance1)) {
-    distance1 = parseFloat(prompt("Ingrese la distancia en metros de la fuente de la primera señal al receptor (Debe ser un número!):"));
-}
-let Qfactor1 = parseFloat(prompt("Ingrese la directividad de la fuente de la primera señal (Q=1 - Emite en todas las direcciones, Q=2 - Emite de forma semiesférica (sólo hacia el frente), Q>2 - A mayor Q, más directiva la fuente):"));
-while (isNaN(Qfactor1)) {
-    Qfactor1 = parseFloat(prompt("Ingrese la directividad de la fuente de la primera señal (Q=1 - Emite en todas las direcciones, Q=2 - Emite de forma semiesférica (sólo hacia el frente), Q>2 - A mayor Q, más directiva la fuente) (Debe ser un número!):"));
-}
-let timeDelay1 = parseFloat(prompt("Ingrese el retraso en segundos de la primera señal:"));
-while (isNaN(timeDelay1)) {
-    timeDelay1 = parseFloat(prompt("Ingrese el retraso en segundos de la primera señal (Debe ser un número!):"));
-}
+
 
 const Signal1 = new Signal(soundPower1, distance1, Qfactor1, timeDelay1);
 
-let newFrequency = parseFloat(prompt("Ingrese una frecuencia para agregarla al espectro de la señal 1, cuando quiera dejar de ingresar frecuencias ingrese 0, un valor negativo o un carácter no numérico:"));
-
-while ( isNaN(newFrequency)==false && newFrequency>0 ) {
-    Signal1.addFrequency(newFrequency);    
-    newFrequency = parseFloat(prompt("Ingrese una frecuencia para agregarla al espectro de la señal 1, cuando quiera dejar de ingresar frecuencias ingrese 0, un valor negativo o un carácter no numérico:"));
-}
-
-let soundPower2 = parseFloat(prompt("Ingrese el nivel de potencia sonora en Watts de la segunda señal:"));
-while (isNaN(soundPower2)) {
-    soundPower2 = parseFloat(prompt("Ingrese el nivel de potencia sonora en Watts de la segunda señal (Debe ser un número!):"));
-}
-let distance2 = parseFloat(prompt("Ingrese la distancia en metros de la fuente de la segunda señal al receptor:"));
-while (isNaN(distance2)) {
-    distance2 = parseFloat(prompt("Ingrese la distancia en metros de la fuente de la segunda señal al receptor (Debe ser un número!):"));
-}
-let Qfactor2 = parseFloat(prompt("Ingrese la directividad de la fuente de la segunda señal (Q=1 - Emite en todas las direcciones, Q=2 - Emite de forma semiesférica (sólo hacia el frente), Q>2 - A mayor Q, más directiva la fuente):"));
-while (isNaN(Qfactor2)) {
-    Qfactor2 = parseFloat(prompt("Ingrese la directividad de la fuente de la segunda señal (Q=1 - Emite en todas las direcciones, Q=2 - Emite de forma semiesférica (sólo hacia el frente), Q>2 - A mayor Q, más directiva la fuente) (Debe ser un número!):"));
-}
-let timeDelay2 = parseFloat(prompt("Ingrese el retraso en segundos de la segunda señal:"));
-while (isNaN(timeDelay2)) {
-    timeDelay2 = parseFloat(prompt("Ingrese el retraso en segundos de la segunda señal (Debe ser un número!):"));
-}
 
 const Signal2 = new Signal(soundPower2, distance2, Qfactor2, timeDelay2);
 
-newFrequency = parseFloat(prompt("Ingrese una frecuencia para agregarla al espectro de la señal 2, cuando quiera dejar de ingresar frecuencias ingrese 0, un valor negativo o un carácter no numérico:"));
-
-while (isNaN(newFrequency)==false && newFrequency>0 ) {
-    Signal2.addFrequency(newFrequency);    
-    newFrequency = parseFloat(prompt("Ingrese una frecuencia para agregarla al espectro de la señal 2, cuando quiera dejar de ingresar frecuencias ingrese 0, un valor negativo o un carácter no numérico:"));
-} 
