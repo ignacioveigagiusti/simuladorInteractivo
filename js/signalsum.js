51a60961f01d01a5f5e87be5e850d9bd28a5af61
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2021-11-27 13:25:53 -0300

    first commit

--- js/signalsum.js
+++ js/signalsum.js
@@ -10,84 +10,84 @@ if (localStorage.getItem("Signal1") === null && localStorage.getItem("Signal1") 
 
 //función Callback para traer señales anteriores
 
-let retrieveLastSignal = document.getElementById("retrieveLastSignal");
-retrieveLastSignal.addEventListener('click',retrieveLastSignalCallback);
+let retrieveLastSignal = $("#retrieveLastSignal");
+retrieveLastSignal.click(retrieveLastSignalCallback);
 function retrieveLastSignalCallback() {
     //Traigo las señales de nuevo por si fueron creadas nuevas señales durante la sesión
     Signal1 = JSON.parse(localStorage.getItem('Signal1'));
     Signal2 = JSON.parse(localStorage.getItem('Signal2'));
     //Asigno valores
-    document.getElementById("soundPower1").value = Signal1.soundPower;
-    document.getElementById("distance1").value = Signal1.distance;
-    document.getElementById("Qfactor1").value = Signal1.Qfactor;
-    document.getElementById("timeDelay1").value = Signal1.timeDelay;
+    $("#soundPower1").value = Signal1.soundPower;
+    $("#distance1").value = Signal1.distance;
+    $("#Qfactor1").value = Signal1.Qfactor;
+    $("#timeDelay1").value = Signal1.timeDelay;
     if (Signal1.frequencies[0] >= 0 && Signal1.frequencies[0] !== null){
-        document.getElementById("frequency1signal1").value = Signal1.frequencies[0]
+        $("#frequency1signal1").value = Signal1.frequencies[0]
     }
     if (Signal1.frequencies[1] >= 0 && Signal1.frequencies[1] !== null){
-        document.getElementById("frequency2signal1").value = Signal1.frequencies[1]
+        $("#frequency2signal1").value = Signal1.frequencies[1]
     }
     if (Signal1.frequencies[2] >= 0 && Signal1.frequencies[2] !== null){
-        document.getElementById("frequency3signal1").value = Signal1.frequencies[2] 
+        $("#frequency3signal1").value = Signal1.frequencies[2] 
     }
     if (Signal1.frequencies[3] >= 0 && Signal1.frequencies[3] !== null){
-        document.getElementById("frequency4signal1").value = Signal1.frequencies[3]      
+        $("#frequency4signal1").value = Signal1.frequencies[3]      
     }
-    document.getElementById("soundPower2").value = Signal2.soundPower;
-    document.getElementById("distance2").value = Signal2.distance;
-    document.getElementById("Qfactor2").value = Signal2.Qfactor;
-    document.getElementById("timeDelay2").value = Signal2.timeDelay;
+    $("#soundPower2").value = Signal2.soundPower;
+    $("#distance2").value = Signal2.distance;
+    $("#Qfactor2").value = Signal2.Qfactor;
+    $("#timeDelay2").value = Signal2.timeDelay;
     if (Signal2.frequencies[0] >= 0 && Signal1.frequencies[0] !== null){
-        document.getElementById("frequency1signal2").value = Signal2.frequencies[0]
+        $("#frequency1signal2").value = Signal2.frequencies[0]
     }
     if (Signal2.frequencies[1] >= 0 && Signal1.frequencies[1] !== null){
-        document.getElementById("frequency2signal2").value = Signal2.frequencies[1]
+        $("#frequency2signal2").value = Signal2.frequencies[1]
     }
     if (Signal2.frequencies[2] >= 0 && Signal1.frequencies[2] !== null){
-        document.getElementById("frequency3signal2").value = Signal2.frequencies[2]
+        $("#frequency3signal2").value = Signal2.frequencies[2]
     }
     if (Signal2.frequencies[3] >= 0 && Signal1.frequencies[3] !== null){
-        document.getElementById("frequency4signal2").value = Signal2.frequencies[3]
+        $("#frequency4signal2").value = Signal2.frequencies[3]
     }
 }
 
 //Función para calcular los parámetros acústicos
 
-let calculate = document.getElementById("calculate");
-calculate.addEventListener('click',calculateCallback);
+let calculate = $("#calculate");
+calculate.click(calculateCallback);
 function calculateCallback() {
     //Tomo datos
-    let soundPower1 = parseFloat(document.getElementById("soundPower1").value);
-    let distance1 = parseFloat(document.getElementById("distance1").value);
-    let Qfactor1 = parseFloat(document.getElementById("Qfactor1").value);
-    let timeDelay1 = parseFloat(document.getElementById("timeDelay1").value);
-    let soundPower2 = parseFloat(document.getElementById("soundPower2").value);
-    let distance2 = parseFloat(document.getElementById("distance2").value);
-    let Qfactor2 = parseFloat(document.getElementById("Qfactor2").value);
-    let timeDelay2 = parseFloat(document.getElementById("timeDelay2").value);
+    let soundPower1 = parseFloat($("#soundPower1").value);
+    let distance1 = parseFloat($("#distance1").value);
+    let Qfactor1 = parseFloat($("#Qfactor1").value);
+    let timeDelay1 = parseFloat($("#timeDelay1").value);
+    let soundPower2 = parseFloat($("#soundPower2").value);
+    let distance2 = parseFloat($("#distance2").value);
+    let Qfactor2 = parseFloat($("#Qfactor2").value);
+    let timeDelay2 = parseFloat($("#timeDelay2").value);
     
     //Chequeo de datos
     if (isNaN(soundPower1) || isNaN(distance1) || isNaN(Qfactor1) || isNaN(timeDelay1) || isNaN(soundPower2) || isNaN(distance2) || isNaN(Qfactor2) || isNaN(timeDelay2)) {
-        document.getElementById("signalSumTag").innerHTML = 'Alguno de los valores ingresados no es correcto!';
-        document.getElementById("signalSumTag").style.color = 'crimson';
-        document.getElementById("signalSumResult").style.display = 'none';
+        $("#signalSumTag").innerHTML = 'Alguno de los valores ingresados no es correcto!';
+        $("#signalSumTag").style.color = 'crimson';
+        $("#signalSumResult").style.display = 'none';
     }
     else{
-        document.getElementById("signalSumTag").innerHTML = 'Suma de Niveles de Presión: ';
-        document.getElementById("signalSumTag").style.color = 'black';
-        document.getElementById("signalSumResult").style.display = 'table-cell';
+        $("#signalSumTag").innerHTML = 'Suma de Niveles de Presión: ';
+        $("#signalSumTag").style.color = 'black';
+        $("#signalSumResult").style.display = 'table-cell';
         //Construyo las señales
         const Signal1 = new Signal(soundPower1, distance1, Qfactor1, timeDelay1);
         const Signal2 = new Signal(soundPower2, distance2, Qfactor2, timeDelay2);
         
-        Signal1.addFrequency(parseInt(document.getElementById('frequency1signal1').value));
-        Signal1.addFrequency(parseInt(document.getElementById('frequency2signal1').value));
-        Signal1.addFrequency(parseInt(document.getElementById('frequency3signal1').value));
-        Signal1.addFrequency(parseInt(document.getElementById('frequency4signal1').value));
-        Signal2.addFrequency(parseInt(document.getElementById('frequency1signal2').value));
-        Signal2.addFrequency(parseInt(document.getElementById('frequency2signal2').value));
-        Signal2.addFrequency(parseInt(document.getElementById('frequency3signal2').value));
-        Signal2.addFrequency(parseInt(document.getElementById('frequency4signal2').value));
+        Signal1.addFrequency(parseInt($('#frequency1signal1').value));
+        Signal1.addFrequency(parseInt($('#frequency2signal1').value));
+        Signal1.addFrequency(parseInt($('#frequency3signal1').value));
+        Signal1.addFrequency(parseInt($('#frequency4signal1').value));
+        Signal2.addFrequency(parseInt($('#frequency1signal2').value));
+        Signal2.addFrequency(parseInt($('#frequency2signal2').value));
+        Signal2.addFrequency(parseInt($('#frequency3signal2').value));
+        Signal2.addFrequency(parseInt($('#frequency4signal2').value));
         
         //Almaceno
         localStorage.setItem('Signal1', JSON.stringify(Signal1));
@@ -95,13 +95,13 @@ function calculateCallback() {
         
         
         //Calculo
-        document.getElementById('signalSumResult').innerHTML = signalsum(Signal1,Signal2);
-        document.getElementById('soundPowerLevel1').value = parseInt(Signal1.soundPowerLevel());
-        document.getElementById('soundPressureLevel1').value = parseInt(Signal1.soundPressureLevel());
-        document.getElementById('soundPressure1').value = parseInt(Signal1.soundPressure());
-        document.getElementById('soundPowerLevel2').value = parseInt(Signal2.soundPowerLevel());
-        document.getElementById('soundPressureLevel2').value = parseInt(Signal2.soundPressureLevel());
-        document.getElementById('soundPressure2').value = parseInt(Signal2.soundPressure());
+        $('#signalSumResult').innerHTML = signalsum(Signal1,Signal2);
+        $('#soundPowerLevel1').value = parseInt(Signal1.soundPowerLevel());
+        $('#soundPressureLevel1').value = parseInt(Signal1.soundPressureLevel());
+        $('#soundPressure1').value = parseInt(Signal1.soundPressure());
+        $('#soundPowerLevel2').value = parseInt(Signal2.soundPowerLevel());
+        $('#soundPressureLevel2').value = parseInt(Signal2.soundPressureLevel());
+        $('#soundPressure2').value = parseInt(Signal2.soundPressure());
     }
 }
 
@@ -109,6 +109,6 @@ function calculateCallback() {
 document.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         event.preventDefault();
-        document.getElementById("calculate").click();
+        $("#calculate").click();
     }
 });
