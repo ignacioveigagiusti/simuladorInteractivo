7acab94991e1dcb981956305ed6a708eb653e0f1
Ignacio Veiga <ignacioveigagiusti@gmail.com>
2021-11-28 18:47:00 -0300

    dBsum box validation

--- js/dBsum.js
+++ js/dBsum.js
@@ -7,10 +7,19 @@ function correlatedSumButtonCallback() {
     if (isNaN(soundPressureLevel1) || isNaN(soundPressureLevel2) || isNaN(phaseDifference)) {
         $("#errorMessage").css('display','block');
         if (isNaN(soundPressureLevel1)) {
-
+            $("#soundPLevel1").css({'border-color':'red','border-style':'solid'})
+        }
+        if (isNaN(soundPressureLevel2)) {
+            $("#soundPLevel2").css({'border-color':'red','border-style':'solid'})
+        }
+        if (isNaN(phaseDifference)) {
+            $("#phaseDifference").css({'border-color':'red','border-style':'solid'})
         }
     }
     else{
+    $("#soundPLevel1").css({'border-color':'','border-style':''})
+    $("#soundPLevel2").css({'border-color':'','border-style':''})
+    $("#phaseDifference").css({'border-color':'','border-style':''})
     $("#errorMessage").css('display','none');
     $('#sumResult').val(correlatedsum(soundPressureLevel1,soundPressureLevel2,phaseDifference));
     }
@@ -23,8 +32,17 @@ function nonCorrelatedSumButtonCallback() {
     let soundPressureLevel2 = parseFloat($("#soundPLevel2").val())
     if (isNaN(soundPressureLevel1) || isNaN(soundPressureLevel2)) {
         $("#errorMessage").css('display','block');
+        if (isNaN(soundPressureLevel1)) {
+            $("#soundPLevel1").css({'border-color':'red','border-style':'solid'})
+        }
+        if (isNaN(soundPressureLevel2)) {
+            $("#soundPLevel2").css({'border-color':'red','border-style':'solid'})
+        }
     }
     else{
+    $("#soundPLevel1").css({'border-color':'','border-style':''})
+    $("#soundPLevel2").css({'border-color':'','border-style':''})
+    $("#phaseDifference").css({'border-color':'','border-style':''})
     $("#errorMessage").css('display','none');
     $('#sumResult').val(noncorrelatedsum(soundPressureLevel1,soundPressureLevel2));
     }
